a11ee59855c43639e8f398d1bb03076f4094c09b
Fernandoh <fernandopulpyto@gmail.com>
2025-11-13 13:06:57 -0600

    Muestra la version del software

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 int main(){
 
     // escrito por fernando herandez valverde
+    std::cout << "SERVIDOR CERRU" << std::endl;
+    
+    std::string app_ver = "0.2";
+    std::cout << "app Version: " << app_ver << std::endl;
 
     std::cout << "Ingresa el puerto: ";
     int puerto = 0;
