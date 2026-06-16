7bcd016c6a3804717f2645cfad11b82a048fccce
fernandohhh <fernandopulpyto@gmail.com>
2026-06-16 06:17:00 +0000

    se cambio el nombre del archivo de configuracion

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 DIR="$(dirname "$0")"
 BIN="$DIR/bin/cerrusv"
-CFG_FILE="cerrusv.cfg"
-PATH_CFG_FILE="/etc/cerrusv.cfg"
+CFG_FILE="cerrucfg.toml"
+PATH_CFG_FILE="/etc/cerrucfg.toml"
 
 echo "### Instalando... ###"
 
@@ -14,16 +14,14 @@ fi
 
 echo "Instalando ejecutable"
 cp "$BIN" /usr/local/bin/
-chmod +x /usr/local/bin/cerrusv
-
-
+#chmod +x /usr/local/bin/cerrusv
 
 if [ ! -f "$CFG_FILE" ]; then
     echo "[ x ] No existe el archvo de configuracion"
 
 elif [ ! -f "$PATH_CFG_FILE" ]; then
     echo "Copiando archivo de configuracion"
-    cp cerrusv.cfg "$PATH_CFG_FILE"
+    cp "$CFG_FILE" "$PATH_CFG_FILE"
 else
     echo "Archivo de configuracion existente en: $PATH_CFG_FILE"
 fi
